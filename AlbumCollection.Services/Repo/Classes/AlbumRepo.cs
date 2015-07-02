bae686037d86dc4b5296d9f1018ee1e054084a50
georgesbilodeau <georgesbilodeau@gmail.com>
2015-07-01 20:18:43 -0400

    Added AlbumRepo

---
+++ AlbumCollection.Services/Repo/Classes/AlbumRepo.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using AlbumCollection.Domain;
+using System.Data.Entity;
+
+namespace AlbumCollection.Services.Repo {
+    public class AlbumRepo : IAlbumRepo {
+        private AlbumContext db;
+
+        public AlbumRepo(AlbumContext db) {
+            this.db = db;
+        }
+
+        #region IAlbumRepo Members
+
+        public async Task<Album> LoadAsync(string id) {
+            return await this.db.Albums
+                .Include(a => a.Artist)
+                .Include(a => a.AlbumArt)
+                .FirstOrDefaultAsync(a => a.MbId == id);
+        }
+
+        #endregion
+
+    }
+}
---
+++ AlbumCollection.Services/Repo/Interfaces/IAlbumRepo.cs
@@ -0,0 +1,8 @@
+﻿using System.Threading.Tasks;
+using AlbumCollection.Domain;
+
+namespace AlbumCollection.Services.Repo {
+    public interface IAlbumRepo {
+        Task<Album> LoadAsync(string id);
+    }
+}
